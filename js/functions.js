4da842c1bd495b85ab592d94a76ba6663f86a268
Jonathon Berardinelli <jberard@rockets.utoledo.edu>
2015-03-29 23:14:33 -0400

    jQuery work added.

--- js/functions.js
+++ js/functions.js
@@ -12,3 +12,33 @@
 	$(this).fadeOut(700);
 	}
 	)
+(function (document, window, undefined) {
+  'use strict';
+  
+  // Find each tooltip
+  var tooltip = $('.tooltip');
+
+  tooltip.each(function() {
+    // Create tooltip element
+    var $this = $(this),
+        tooltipText = $('<div class="tooltip-text">' + $this.data('tooltip-text') + '</div>');
+      
+    // Add tooltip to body on mouse over
+    $this.on('mouseover', function() {
+        tooltipText.appendTo('body');
+    });
+  
+    // Remove tooltip on mouseout
+    $this.on('mouseout', function() {
+        tooltipText.remove();
+    });
+  
+    // Attach the tooltip to the mouse cursor
+    $this.on('mousemove', function(e) {
+        tooltipText.css('top', (e.pageY + 20) + 'px');
+        tooltipText.css('left', (e.pageX + 20) + 'px');
+    });
+  
+  });
+  
+})(document, window);
