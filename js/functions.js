2a4952182e912e4c397b1ad8cac3df9c1d142fdc
Jonathon Berardinelli <jberard@rockets.utoledo.edu>
2015-04-29 17:26:27 -0400

    Final Prjoect initial changes.

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,3 @@
-	$function () {
-	$('h2').hide().slideDown();
-	var $li = $('li');
-	$li.hide().each(function(index) 
-	{
-	$(this).delay(700 * index).fadeIn(700);
-	}
-	);
-	};
-	$li.on('click' , fuction()
-	{
-	$(this).fadeOut(700);
-	}
-	)
 (function (document, window, undefined) {
   'use strict';
   
